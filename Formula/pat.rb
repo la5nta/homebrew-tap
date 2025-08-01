f5dcfd558e0fafd92bc399b6e6e826150a0a731e
Martin Hebnes Pedersen <martin.h.pedersen@gmail.com>
2025-08-01 07:37:29 +0200

    Pat v0.18.0

--- Formula/pat.rb
+++ Formula/pat.rb
@@ -1,7 +1,7 @@
 class Pat < Formula
   desc "Send and receive email over Winlink"
   homepage "https://github.com/la5nta/pat"
-  url "https://github.com/la5nta/pat.git", tag: "v0.17.0"
+  url "https://github.com/la5nta/pat.git", tag: "v0.18.0"
   license "MIT"
   head "https://github.com/la5nta/pat.git", branch: "develop"
 
